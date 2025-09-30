a49d9a6abac8b7a477a0bd6f57d6d4bc970e4ce6
Vitor Gomes <vitorgomes_silva@outlook.com>
2025-09-30 15:53:38 -0300

    First commit

---
+++ AmoraProject/AmoraProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  AmoraProjectApp.swift
+//  AmoraProject
+//
+//  Created by Vitor Gomes on 30/09/25.
+//
+
+import SwiftUI
+
+@main
+struct AmoraProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
